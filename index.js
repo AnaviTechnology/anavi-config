3599e93a66bc4eaf5b49db4119d414271a4f592a
Leon Anavi <leon@anavi.org>
2016-02-15 00:57:28 +0200

    index.js: handle POST request
    Process HTTP POST and return a response.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/ui'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/save', function(req, res) {
+	console.log("post request detected");
+	var result = { error: 0, errCode: '' }
+	res.json(result);
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
