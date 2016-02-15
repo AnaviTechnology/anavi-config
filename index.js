95af2fa40f7d19c1b1bb66de0af8b4970522c909
Leon Anavi <leon@anavi.org>
2016-02-16 01:29:32 +0200

    index.js: change port to 80
    Change port of the web server to 80.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ app.post('/save', function(req, res) {
 	res.json(result);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 });
-
